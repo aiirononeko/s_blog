49308d0d33efbf7f1e871aea39e81248034d1b36
片田凌太 <aiirononeko@gmail.com>
2020-02-19 22:42:44 +0900

    new postsの一覧表示機能を実装

---
+++ components/top/Posts.vue
@@ -0,0 +1,78 @@
+<template>
+  <el-col :span="8" style="margin-bottom: 25px;">
+    <el-card class="box-card" style="margin: 0 auto;">
+      <div class="box-card-items">
+        <ul style="padding: 0;">
+          <li v-for="(tag, i) in tags" :key="i" class="tag">{{ tag.fields.name }}</li>
+        </ul>
+        <h2 class="post-title">{{ title }}</h2>
+        <p class="post-date">{{ getDate() }}</p>
+
+        <!-- classをバインディングすると警告でるの何でだろ -->
+        <el-image :src="image.fields.file.url" :class="top-image">
+          <div slot="placeholder" class="image-slot">
+            Loading<span class="dot">...</span>
+          </div>
+        </el-image>
+      </div>
+    </el-card>
+  </el-col>
+</template>
+
+<script>
+export default {
+  props: {
+    title: null,
+    image: null,
+    date: null,
+    tags: null,
+  },
+  methods: {
+    getDate() {
+      const date = new Date(this.date)
+      const year = date.getFullYear()
+      const month = date.getMonth() + 1
+      const day = date.getDate()
+      return year + '/' + month + '/' + day
+    }
+  }
+}
+</script>
+
+<style>
+  .top-image {
+    width: 295px;
+    height: 176px;
+  }
+
+  .box-card {
+    font-family: "M PLUS 1p";
+    width: 350px;
+    height: 405px;
+  }
+
+  .box-card-items {
+    margin: auto;
+  }
+
+  .tag {
+    list-style: none;
+    text-align: center;
+    font-size: 14px;
+    margin-bottom: 20px;
+    color: #696969;
+  }
+
+  .post-title {
+    font-size: 16px;
+    text-align: center;
+    margin-bottom: 30px;
+  }
+
+  .post-date {
+    font-size: 12px;
+    text-align: center;
+    margin-bottom: 15px;
+    color: #808080;
+  }
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,30 +1,55 @@
 <template>
-  <div>
-    <template v-if="posts.length">
-      <ul v-for="(post, i) in posts" :key="i">
-        <li>{{ post.fields.title }}</li>
-        <ul>
-          <li>{{ post.fields.body }}</li>
-          <li>{{ post.fields.publishDate }}</li>
-        </ul>
-      </ul>
-    </template>
-    <template v-else>
-      投稿された記事はありません。
-    </template>
-  </div>
+  <el-container>
+    <el-header>
+      <Header/>
+    </el-header>
+    <el-container style="width: 80%; margin: 0 auto;">
+      <el-main>
+        <h2 style="text-align: center; margin: 30px 0;">New Posts</h2>
+        <el-row>
+          <Posts
+            v-for="(post, i) in posts"
+            :key="i"
+            :title="post.fields.title"
+            :image="post.fields.headerImage"
+            :date="post.fields.publishDate"
+            :tags="post.fields.tags"
+          />
+        </el-row>
+        <h2 style="text-align: center; margin: 30px 0;">Popular Tags</h2>
+        <Tags
+        />
+      </el-main>
+    </el-container>
+    <el-footer>
+      <Footer/>
+    </el-footer>
+  </el-container>
 </template>
 
 <script>
 import client from '~/plugins/contentful'
+
+import Header from '~/components/shared/Header'
+import Posts from '~/components/top/Posts'
+import Tags from '~/components/top/Tags'
+import Footer from '~/components/shared/Footer'
 export default {
-  async asyncData({ env }) {
-    let posts = []
-    await client.getEntries({
-      content_type: env.CTF_BLOG_POST_TYPE_ID,
-      order: '-fields.publishDate'
-    }).then(res => (posts = res.items)).catch(console.error)
-    return { posts }
+  components: {
+    Header,
+    Posts,
+    Tags,
+    Footer
+  },
+  asyncData({ env }) {
+    return client
+      .getEntries({ content_type: env.CTF_BLOG_POST_TYPE_ID })
+      .then(entries => {
+        return {
+          posts: entries.items
+        }
+      })
+      .catch(console.error)
   }
 }
 </script>
