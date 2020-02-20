e4a26f2b6ac579998ddecfd712c403118d0c9009
片田凌太 <aiirononeko@gmail.com>
2020-02-20 12:26:42 +0900

    aboutとcontactのリンクを追加

---
+++ pages/contact.vue
@@ -0,0 +1,24 @@
+<template>
+  <el-container>
+    <el-header>
+      <Header/>
+    </el-header>
+    <el-container style="width: 80%; margin: 0 auto;">
+      <el-main style="margin-bottom: 50px;">
+        <h2 style="text-align: center; margin: 40px 0;">Contact Me!</h2>
+      </el-main>
+    </el-container>
+    <el-footer>
+      <Footer/>
+    </el-footer>
+  </el-container>
+</template>
+
+<script>
+  import Header from '~/components/shared/Header'
+  export default {
+    components: {
+      Header
+    }
+  }
+</script>
