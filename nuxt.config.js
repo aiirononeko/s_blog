8d974a7787649f60bb66caaa89f22dc07e7b95bd
片田凌太 <aiirononeko@gmail.com>
2020-02-20 18:27:27 +0900

    header情報を編集

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,11 +5,11 @@ export default {
   ** Headers of the page
   */
   head: {
-    title: process.env.npm_package_name || '',
+    title: 'Excelsior',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+      { hid: 'description', name: 'description', content: 'メンズ美容に関する情報を発信しているブログです。化粧品会社で働く22歳(♂)が試したモノ、コト' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
